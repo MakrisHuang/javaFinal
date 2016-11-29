249ac386b79df3291dc4be684859e5949ee12765
Makris Huang <vallwesture@gmail.com>
2016-11-29 19:13:16 +0800

    Add bluetooth adapter and button

--- BikeFinder/app/src/main/java/com/example/makris/bikefinder/MainActivity.java
+++ BikeFinder/app/src/main/java/com/example/makris/bikefinder/MainActivity.java
@@ -20,10 +20,17 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3, b4;
+    // create adapter
+    private BluetoothAdapter bluetoothAdapter;
+    private Set<BluetoothDevice> pariedDevices;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R);
     }
 }
