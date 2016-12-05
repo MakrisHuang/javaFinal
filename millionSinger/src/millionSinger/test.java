18d76abe3feffea56dab6f83964e417f85ab7214
Makris Huang <vallwesture@gmail.com>
2016-12-05 13:49:00 +0800

    Create new java project

---
+++ millionSinger/src/millionSinger/test.java
@@ -0,0 +1,7 @@
+package millionSinger;
+
+public class test {
+	public static void main(String args[]){
+		System.out.println("Test");
+	}
+}
--- src/javaFinal/test.java
+++
@@ -1,7 +0,0 @@
-package javaFinal;
-
-public class test {
-	public static void main(String args[]){
-		System.out.println("Final Project GOGO!!!!");
-	}
-}
