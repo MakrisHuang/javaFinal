769209a455d5e55d80b5d287eaf9a21eb4414124
Makris Huang <vallwesture@gmail.com>
2016-12-21 11:35:26 +0800

    增加updateState(), 金錢顏色會變

--- millionSinger/src/millionSinger/Player.java
+++ millionSinger/src/millionSinger/Player.java
@@ -16,11 +16,14 @@ import javax.swing.SwingConstants;
 public class Player extends JFrame {
 	
 	// UI declaration
-	private final JLabel NT_50,NT_40,NT_30,NT_20;
-	private final JButton	million,btn1,btn2,btn3;
+	private JLabel NT_30,NT_20,NT_10,NT_0;
+	private JButton	million,btn1,btn2,btn3;
 	//Color 
 	private final Color color_usable_btn = Color.getHSBColor(0.60f,1.0f,0.8f);
 	private final Color color_useless_btn = Color.getHSBColor(0.60f,0.3f,0.8f);
+	private final Color color_NT_label = Color.getHSBColor(0.54f,1.0f,0.8f);
+	private final Color color_selected_NT_label = Color.pink;
+	
 	// variable declaration
 	private final String[] Category_List = {"百萬大歌星","周杰倫精選","KTV精選","最洗腦歌曲"};
 	private final String[] Song_List_type0 = {"million1","million2","million3"};
@@ -28,30 +31,32 @@ public class Player extends JFrame {
 	private final String[] Song_List_type2 = {"Song4","Song5","Song6"};
 	private final String[] Song_List_type3 = {"Song7","Song8","Song9"};
 	private final String[][] Song_List = {Song_List_type0,Song_List_type1,Song_List_type2,Song_List_type3};
+	
 	// Game control variables
 	private int[] Usable_Button_List = {0,1,1,1}; // to record which category can play
 	private int[] Usable_Help_List = {1,1,1};
 	private int selected_category = -1;
 	private int song_index = -1;
 	private int game_stage = -1;
+	
 	//ActionListener
 	private final categoryHandler category_handler = new categoryHandler();
 	private final selectSongHandler song_handler = new selectSongHandler();
-	// User variables
-	private int moneyEarned = 0;
-	private int moneyOfStage[] = {20, 30, 40, 50}; 
 	
-	private void initState(){
+	// User variables
+	private int moneyEarnedIndex = 0;	// max = 3
+	private int moneyOfStage[] = {0, 10, 20, 30}; 
+	
+	private void initGameStage(){
 		this.game_stage = 1;
 	}
 	
 	public Player(){
 		super("Million Singer");
-		// initialize environment
-		this.initState();
+		
+		this.initGameStage();
 		
 		this.setLayout(new GridBagLayout());
-		//
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.weightx = 1; gbc.weighty = 1; // whether change size with frame
 		gbc.fill = GridBagConstraints.NONE; // whether auto fill gbc
@@ -60,8 +65,6 @@ public class Player extends JFrame {
 		gbc.gridx = 0; gbc.gridy = 0; // location of gbc
 		gbc.gridwidth = 10; gbc.gridheight = 3; // size of gbc , this is for title
 		million = new JButton(Category_List[0]);
-		million.setForeground(Color.WHITE);
-		million.setOpaque(true);
 		million.setFont(new Font("新細明體", Font.BOLD,48));
 		million.setPreferredSize(new Dimension(600,60));
 		million.setBackground(color_useless_btn);
@@ -71,48 +74,46 @@ public class Player extends JFrame {
 		gbc.gridx = 0;
 		
 		gbc.gridy = 3;
-		NT_50 = new JLabel("NT$50",SwingConstants.CENTER);
-		NT_50.setOpaque(true);
-		NT_50.setForeground(Color.WHITE);
-		NT_50.setFont(new Font("Calibri", Font.BOLD,36));
-		NT_50.setPreferredSize(new Dimension(200,40));
-		NT_50.setBackground(Color.getHSBColor(0.54f,1.0f,0.8f));
-		add(NT_50,gbc);
-	
-		gbc.gridy = 6;
-		NT_40 = new JLabel("NT$40",SwingConstants.CENTER);
-		NT_40.setOpaque(true);
-		NT_40.setForeground(Color.WHITE);
-		NT_40.setFont(new Font("Calibri", Font.BOLD,36));
-		NT_40.setPreferredSize(new Dimension(200,40));
-		NT_40.setBackground(Color.getHSBColor(0.54f,1.0f,0.8f));
-		add(NT_40,gbc);
-		
-		gbc.gridy = 9;
-		NT_30 = new JLabel("NT$30",SwingConstants.CENTER);
+		NT_30 = new JLabel(String.format("NT$%d", moneyOfStage[3]),SwingConstants.CENTER);
 		NT_30.setOpaque(true);
 		NT_30.setForeground(Color.WHITE);
 		NT_30.setFont(new Font("Calibri", Font.BOLD,36));
 		NT_30.setPreferredSize(new Dimension(200,40));
-		NT_30.setBackground(Color.getHSBColor(0.54f,1.0f,0.8f));
+		NT_30.setBackground(color_NT_label);
 		add(NT_30,gbc);
-		
-		gbc.gridy = 12;
-		NT_20 = new JLabel("NT$30",SwingConstants.CENTER);
+	
+		gbc.gridy = 6;
+		NT_20 = new JLabel(String.format("NT$%d", moneyOfStage[2]),SwingConstants.CENTER);
 		NT_20.setOpaque(true);
 		NT_20.setForeground(Color.WHITE);
 		NT_20.setFont(new Font("Calibri", Font.BOLD,36));
 		NT_20.setPreferredSize(new Dimension(200,40));
-		NT_20.setBackground(Color.getHSBColor(0.54f,1.0f,0.8f));
+		NT_20.setBackground(color_NT_label);
 		add(NT_20,gbc);
+		
+		gbc.gridy = 9;
+		NT_10 = new JLabel(String.format("NT$%d", moneyOfStage[1]),SwingConstants.CENTER);
+		NT_10.setOpaque(true);
+		NT_10.setForeground(Color.WHITE);
+		NT_10.setFont(new Font("Calibri", Font.BOLD,36));
+		NT_10.setPreferredSize(new Dimension(200,40));
+		NT_10.setBackground(color_NT_label);
+		add(NT_10,gbc);
+		
+		gbc.gridy = 12;
+		NT_0 = new JLabel(String.format("NT$%d", moneyOfStage[0]),SwingConstants.CENTER);
+		NT_0.setOpaque(true);
+		NT_0.setForeground(Color.WHITE);
+		NT_0.setFont(new Font("Calibri", Font.BOLD,36));
+		NT_0.setPreferredSize(new Dimension(200,40));
+		NT_0.setBackground(color_selected_NT_label);
+		add(NT_0,gbc);
 		
 		gbc.gridwidth = 6 ; gbc.gridheight = 4;
 		gbc.gridx = 4;
 		
 		gbc.gridy = 3;
 		btn1 = new JButton(Category_List[1]);
-		btn1.setOpaque(true);
-		btn1.setForeground(Color.WHITE);
 		btn1.setFont(new Font("新細明體", Font.BOLD,40));
 		btn1.setPreferredSize(new Dimension(300,60));
 		btn1.setBackground(color_useless_btn);
@@ -120,8 +121,6 @@ public class Player extends JFrame {
 		
 		gbc.gridy = 8;
 		btn2 = new JButton(Category_List[2]);
-		btn2.setOpaque(true);
-		btn2.setForeground(Color.WHITE);
 		btn2.setFont(new Font("新細明體", Font.BOLD,40));
 		btn2.setPreferredSize(new Dimension(300,60));
 		btn2.setBackground(color_useless_btn);
@@ -129,8 +128,6 @@ public class Player extends JFrame {
 		
 		gbc.gridy = 12;
 		btn3 = new JButton(Category_List[3]);
-		btn3.setOpaque(true);
-		btn3.setForeground(Color.WHITE);
 		btn3.setFont(new Font("新細明體", Font.BOLD,40));
 		btn3.setPreferredSize(new Dimension(300,60));
 		btn3.setBackground(color_useless_btn);
@@ -171,27 +168,80 @@ public class Player extends JFrame {
 			if (e.getSource() == btn2) song_index = 2;
 			if (e.getSource() == btn3) song_index = 3;	
 			
-			btn1.setText(Category_List[1]);
-			btn2.setText(Category_List[2]);
-			btn3.setText(Category_List[3]);
-			
-			btn1.removeActionListener(song_handler);
-			btn2.removeActionListener(song_handler);
-			btn3.removeActionListener(song_handler);
-			
+			triggerMVPlayer(song_index);
 			//enableButton(Usable_Button_List);
 		}
+	}
+	
+	public void triggerMVPlayer(int song_index){
+		// new the MV Player here
+		System.out.println(String.format("selected song: %d", song_index));
+		
+		updateState(true);
+	}
+	
+	// when new round, update stage
+	private void updateState(boolean wonTheGame){
+		this.game_stage += 1;
+		System.out.printf("New stage: %d\n", this.game_stage);
+		
+		// 更新錢的數量
+		if (wonTheGame && (moneyEarnedIndex + 1) <= moneyOfStage.length)	
+			moneyEarnedIndex += 1;
+		
+		// 選定目前所獲得的錢
+		switch (moneyEarnedIndex){
+			case 0:
+				NT_30.setBackground(color_NT_label);
+				NT_20.setBackground(color_NT_label);
+				NT_10.setBackground(color_NT_label);
+				NT_0.setBackground(color_selected_NT_label);
+				break;
+			case 1:
+				NT_30.setBackground(color_NT_label);
+				NT_20.setBackground(color_NT_label);
+				NT_10.setBackground(color_selected_NT_label);
+				NT_0.setBackground(color_NT_label);
+				break;
+			case 2: 
+				NT_30.setBackground(color_NT_label);
+				NT_20.setBackground(color_selected_NT_label);
+				NT_10.setBackground(color_NT_label);
+				NT_0.setBackground(color_NT_label);
+				break;
+			case 3:
+				NT_30.setBackground(color_selected_NT_label);
+				NT_20.setBackground(color_NT_label);
+				NT_10.setBackground(color_NT_label);
+				NT_0.setBackground(color_NT_label);
+				break;
+			default:
+				break;
+		}
+		
+		// after finish the game, reset all variables
+		resetEnvirVaris();
 	}
 	
 	// update state
 	private void resetEnvirVaris(){
 		this.selected_category = -1;
 		this.song_index = -1;
-	}
-	
-	// when new round, update stage
-	private void updateState(int stage, int newMoney){
-		this.game_stage += 1;
+		
+		// 重新設定類別名稱
+		btn1.setText(Category_List[0]);
+		btn2.setText(Category_List[1]);
+		btn3.setText(Category_List[2]);
+		
+		// 移除song_handler
+		btn1.removeActionListener(song_handler);
+		btn2.removeActionListener(song_handler);
+		btn3.removeActionListener(song_handler);
+		
+		// 重新加入category_handler
+		btn1.addActionListener(category_handler);
+		btn2.addActionListener(category_handler);
+		btn3.addActionListener(category_handler);
 	}
 	
 	// change Usable_Button_List -> use it when a category finished
@@ -231,10 +281,10 @@ public class Player extends JFrame {
 		btn1.setVisible(false);
 		btn2.setVisible(false);
 		btn3.setVisible(false);
-		NT_50.setVisible(false);
-		NT_40.setVisible(false);
 		NT_30.setVisible(false);
 		NT_20.setVisible(false);
+		NT_10.setVisible(false);
+		NT_0.setVisible(false);
 	}
 	// show items in the Frame
 	public void showPlayer(){
@@ -242,10 +292,10 @@ public class Player extends JFrame {
 		btn1.setVisible(true);
 		btn2.setVisible(true);
 		btn3.setVisible(true);
-		NT_50.setVisible(true);
-		NT_40.setVisible(true);
 		NT_30.setVisible(true);
 		NT_20.setVisible(true);
+		NT_10.setVisible(true);
+		NT_0.setVisible(true);
 	}
 	
 	
